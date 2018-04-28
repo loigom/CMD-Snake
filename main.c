41289b298c17b04a9868474e8d4fc4fdcbfb8470
mothrakk <mothrakk1@gmail.com>
2018-04-28 04:31:07 +0300

    Added game over screen

--- main.c
+++ main.c
@@ -1,16 +1,4 @@
-/*
-
-TODO
-
-1. Improve documentation and consistency.
-2. Console window size needs checking.
-3. Always show score while playing.
-4. Make the game go faster as the score gets higher.
-
-*/
-
-
-// Dependencies and global constants
+/* Dependencies and global constants */
 #include <stdio.h>
 #include <time.h>
 #include <conio.h>
@@ -18,7 +6,7 @@ TODO
 
 #define RES_X 40
 #define RES_Y 20
-#define DELAY_MS 100
+#define DELAY_MS 90
 
 #define LEFT 1
 #define RIGHT 2
@@ -27,7 +15,7 @@ TODO
 
 #define FOOD_SYMBOL 'O'
 
-// Structures
+/* Structures */
 struct Coords
 {
 	int x, y;
@@ -42,53 +30,88 @@ struct Snake
 	char symbol;
 };
 
-// Functions and the rest
-
+/* Function headers */
 // Write to target console buffer using coordinates
+void c_write(int x, int y, char c);
+// Read from target console buffer using coordinates
+char c_read(int x, int y);
+// Draw the initial grid, initialize structures, initial drawing
+void setup(struct Snake *snake, struct Coords *food);
+// Process input, if keyboard was hit
+void input(struct Snake *snake);
+// Logic
+int process(struct Snake *snake, struct Coords *food);
+// Program end
+void end(void);
+
+/* */
+int main(void)
+{
+	int playing = 1;
+	struct Snake snake;
+	struct Coords food;
+	setup(&snake, &food);
+
+	while (playing)
+	{
+		input(&snake);
+		playing = process(&snake, &food);
+		Sleep(DELAY_MS);
+	}
+
+	end();
+}
+
 void c_write(int x, int y, char c)
 {
 	COORD coord = { x, y };
+	COORD bottom_left = { 0, RES_Y + 1 };
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleCursorPosition(h, coord);
 	putchar(c);
 }
-// Read from target console buffer using coordinates
+
 char c_read(int x, int y)
 {
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 	char out;
-	DWORD len = sizeof(TCHAR);
 	COORD coord = { x, y };
 	long unsigned int num_read;
-
-	ReadConsoleOutputCharacter(h, &out, len, coord, &num_read);
+	ReadConsoleOutputCharacter(h, &out, 1, coord, &num_read);
 	return out;
 }
 
 void setup(struct Snake *snake, struct Coords *food)
 {
+	// Seed the random integer generator
+	srand(time(NULL));
 	// Draw the initial grid
 	system("cls");
 	int x, y;
+	// Top horizontal border
 	for (x = 0; x < RES_X; x++)
 	{
 		putchar('#');
 	}
 	putchar('\n');
-	for (y = 1; y < RES_Y-1; y++)
+	// Sides
+	for (y = 1; y < RES_Y - 1; y++)
 	{
 		putchar('#');
-		for (x = 1; x < RES_X-1; x++)
+		for (x = 1; x < RES_X - 1; x++)
 		{
 			putchar(' ');
 		}
 		putchar('#');
 		putchar('\n');
 	}
+	// Bottom horizontal border
 	for (x = 0; x < RES_X; x++)
 	{
 		putchar('#');
 	}
+	putchar('\n');
+	puts("Score: 0");
 
 	// Configure the snake
 	snake->head.x = RES_X / 2;
@@ -175,8 +198,8 @@ int process(struct Snake *snake, struct Coords *food)
 			break;
 	}
 	// Now read the console at these new coordinates
-	// Did the snek drive into itself or a wall?
 	c = c_read(snake->head.x, snake->head.y);
+	// Did the snek drive into itself or a wall?
 	if (c != ' ' && c != FOOD_SYMBOL)
 	{
 		playing = 0;
@@ -187,30 +210,30 @@ int process(struct Snake *snake, struct Coords *food)
 		snake->tail_len++;
 		food->x = rand() % (RES_X - 2) + 1; // Taking the borders into consideration
 		food->y = rand() % (RES_Y - 2) + 1;
-		c_write(food->x, food->y, FOOD_SYMBOL);
+		// Update score
+		char converted[5] = { 0 };
+		itoa(snake->tail_len, converted, 10);
+		i = 0;
+		while (converted[i])
+		{
+			c_write(6 + i, RES_Y, converted[i++]);
+		}
 	}
 	c_write(snake->head.x, snake->head.y, snake->symbol);
+	c_write(food->x, food->y, FOOD_SYMBOL);
 	return playing;
 }
 
-int main(void)
+void end(void)
 {
-	srand(time(NULL));
-	int playing = 1;
-	struct Snake snake;
-	struct Coords food;
-	setup(&snake, &food);
-
-	while (playing)
+	// Print "game over" to bottom-right
+	char over[] = "Game over";
+	int i = strlen(over) - 1;
+	int x = RES_X - 1;
+	for (; i >= 0; i--)
 	{
-		input(&snake);
-		playing = process(&snake, &food);
-		Sleep(DELAY_MS);
+		c_write(x--, RES_Y, over[i]);
 	}
-
-	system("cls");
-	puts("Game over\n");
-	printf("Score: %d\n", snake.tail_len);
-	printf("Exiting in 5s\n");
-	Sleep(5000);
+	// Move cmd cursor to bottom
+	c_write(0, RES_Y + 1, ' ');
 }
